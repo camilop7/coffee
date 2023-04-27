87452be0aa0851cc85f3aa7076e092ab71c31c39
camilo palacios <camilop7@icloud.com>
2023-04-27 08:02:38 +0100

    style changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  Rails.application.routes.draw do
+    root to: "pages#home"
+    get "", to: "pages#"
+  end
+
 end
