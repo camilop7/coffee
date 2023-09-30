3409676e52a4179bd27a150dcb928cd1a9e68dbb
camilo palacios <camilop7@icloud.com>
2023-09-30 20:51:33 +0100

    refactored to scss components

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+def show_card
+  @image_url = "https://res.cloudinary.com/difj9msh3/image/upload/v1695482480/IMG_0425_xtbfav.heic"
+  @alt_text = "Alt Text for Image"
+  @description = "Description of the picture"
+  @link = "#"
+end
+
 end
